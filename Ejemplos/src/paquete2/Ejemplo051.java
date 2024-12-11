21ff8c7b4a45501fc39710367441fc055d901909
Kenny Paredes <jkparedes3@utpl.edu.ec>
2024-12-11 12:17:45 -0500

    Ejemplo051 git commit -a -mEjemplo051 git commit -a -mEjemplo051

---
+++ Ejemplos/src/paquete2/Ejemplo051.java
@@ -0,0 +1,38 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package paquete2;
+
+import java.util.Scanner;
+
+
+
+/**
+ *
+ * @author reroes
+ */
+public class Ejemplo051 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // Declarar y generar arreglos en Java
+Scanner entrada = new Scanner(System.in); 
+        // arreglo de tipo entero
+        System.out.println("iNGRESE EL NUMERO DE ELEMENTOS");
+        int numero_elementos = entrada.nextInt();
+        
+        int[]arreglo1 = new int[numero_elementos]; 
+        
+        // Se hace uso del ciclo repetitivo for para recorrer el arreglo
+        for (int i = 0; i < arreglo1.length; i++) {
+            System.out.printf("Subíndice o Índice %d, con valor %d\n",
+                    i, arreglo1[i]);
+        }
+        
+    }
+
+}
